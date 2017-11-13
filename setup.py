26582396bdd9ce89d6419c35481f80b49c88d896
Justin Bass <justinalanbass@gmail.com>
2017-11-12 21:09:09 -0800

    Pypi initial config

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'colorsafe',
+  packages = ['colorsafe'],
+  version = '0.1.0.dev1',
+  description = 'A colorized data storage scheme for printing on paper.',
+  long_description = 'A colorized data storage scheme for printing on paper.',
+  author = 'Justin Bass',
+  author_email = 'colorsafeproject@gmail.com',
+  url = 'https://github.com/colorsafe/colorsafe',
+  download_url = 'https://github.com/colorsafe/colorsafe/archive/0.1.tar.gz',
+  keywords = ['colorized', 'data', 'storage', 'paper', 'printing'],
+  classifiers = [],
+)
